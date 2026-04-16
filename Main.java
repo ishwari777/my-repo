1eb88c8308d8e2d0b80e75c399365f8ac3e3f914
Ishwari thorat <ishwarithorat0011@gmail.com>
2026-04-16 13:23:54 +0530

    updated file

--- Main.java
+++ Main.java
@@ -8,5 +8,10 @@ public class Main {
         System.out.println("a - b = " + (a - b));
         System.out.println("a * b = " + (a * b));
         System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        
+        // Additional operations
+        System.out.println("a^2 = " + (a * a));
+        System.out.println("b^2 = " + (b * b));
     }
 }
