34b05ada5c36507d62eaad1adbe5a1731dff643b
Ishwari thorat <ishwarithorat0011@gmail.com>
2026-04-16 13:20:56 +0530

    java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Simple Calculator
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
