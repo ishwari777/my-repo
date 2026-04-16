f917cdf786bbea324024315c188b3d4d14a87967
Ishwari thorat <ishwarithorat0011@gmail.com>
2026-04-16 12:56:23 +0530

    python files

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple Python script example
+"""
+
+def main():
+    print("Hello, World!")
+    print("This is a basic Python file.")
+
+if __name__ == "__main__":
+    main()
