40312a91bcdb680869554983304a0890e01476df
Ishwari777 <ishwarithorat0011@gmail.com>
2026-04-29 18:41:09 +0530

    add me

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Simple Python script example
 
 def main():
     print("Hello, World!")
-    print("This is a basic Python file.")
+    print("This is Python file.")
 
 if __name__ == "__main__":
     main()
